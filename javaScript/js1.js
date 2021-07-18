4336bf37cf46dcecee369a752ea5c94bef66928a
ARUNKUMAR <kumararunara5599@gmail.com>
2021-07-18 08:48:50 +0530

    newFirst

---
+++ javaScript/js1.js
@@ -0,0 +1,33 @@
+console.log('hello')
+console.time('kl')
+console.log([7,9,9])
+console.log({arun:"this",kumar:"bhai"})
+console.table({this :"arun", yarr:"kumar"})
+console.warn('this warn')
+// console.clear();
+console.timeEnd('kl')
+console.assert(456 < 90 ,'age is not');
+console.error('hiii');
+var name="arun";
+console.log(name);
+let nu=90;
+nu=87;
+console.log(nu);
+const n1=900;
+ 
+console.log(n1);
+{
+    let n=90;
+    console.log(n);
+}
+const arr=[7,89];
+arr.push(78);
+arr[0]=890;
+console.log(arr)
+var name="arun";
+console.log(name)
+console.log(typeof name)
+let marks=90;
+console.log(typeof marks++ +" "+marks++)
+let v1=null;
+console.log(typeof v1 +" "+typeof arr);
